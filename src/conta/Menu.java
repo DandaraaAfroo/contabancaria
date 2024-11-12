3f5e2e0cf5a6453391ada256a5eb839327a95d97
unknown <dandarad196@gmail.com>
2024-11-12 15:59:42 -0300

    Task- 3

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,16 +1,50 @@
 package conta;
 
 import java.util.Scanner;
-
+import conta.Util.Cores;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		//Testando a classe Conta
+		
+		Conta c1 = new Conta(1, 123, 1, "Dandara", 1000.0f);
+		c1.visualizar();
+		c1.sacar(1200.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
+		
+		// Testando a classe Contacorrente
+		
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 1500.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(1200.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		   
+		
+		// Teste da Classe ContaPoupanca
+		
+		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Victor", 10000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
 
 		Scanner leitor = new Scanner(System.in);
 
 		int opcao;
 
 		while (true) {
+			
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
 
 			System.out.println("***************************************************************************");
 			System.out.println("                                                                           ");
